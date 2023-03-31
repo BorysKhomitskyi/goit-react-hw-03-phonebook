e69056cbc06750a4a1cfb83651866223b4b1aa43
BorysKhomitskyi <borys2130@gmail.com>
2023-03-31 19:29:11 +0300

    goit-react-hw-03-phonebook

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,84 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import css from './ContactForm.module.css';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  nameId = nanoid();
+  numberId = nanoid();
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit({
+      name: this.state.name,
+      number: this.state.number,
+    });
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form className={css.form} onSubmit={this.handleSubmit}>
+        <label className={css.label} htmlFor={this.nameId}>
+          <span className={css.title}>Name</span>
+          <input
+            className={css.input}
+            id={this.nameId}
+            type="text"
+            value={name}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. 
+            For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <label className={css.label} htmlFor={this.numberId}>
+          Number
+          <input
+            className={css.input}
+            id={this.numberId}
+            type="tel"
+            value={number}
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, 
+            dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <button className={css.button} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
